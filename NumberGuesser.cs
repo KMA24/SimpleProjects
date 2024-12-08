e4ddb79e82e38ce111c26e2851e8babd3f983b74
KMA24 <kianmandersen2341@gmail.com>
2024-12-08 13:58:58 +0100

    Add files via upload
    Added NumberGuesser game

---
+++ NumberGuesser.cs
@@ -0,0 +1,19 @@
+﻿Random rnd = new Random();
+
+int RandomNumber = rnd.Next(0, 10); // Picks a number from 0-10
+
+Console.WriteLine("Guess The Number From 0-10!");
+
+int PickedNumber = Int32.Parse(Console.ReadLine()); // Converts the guess from string to int
+
+if (RandomNumber == PickedNumber)
+{
+    Console.WriteLine("You Guessed The Number!");
+}
+
+else
+{
+    Console.WriteLine("You Didnt Guess the Correct Number!");
+    Console.WriteLine("The Correct Number was " + RandomNumber);
+}
+
