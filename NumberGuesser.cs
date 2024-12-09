bb8cd2a3b7bf0f767e55337d61995674cc9bf83e
KMA24 <kianmandersen2341@gmail.com>
2024-12-09 01:05:08 +0100

    Added Exception

--- NumberGuesser.cs
+++ NumberGuesser.cs
@@ -4,16 +4,28 @@ int RandomNumber = rnd.Next(0, 10); // Picks a number from 0-10
 
 Console.WriteLine("Guess The Number From 0-10!");
 
-int PickedNumber = Int32.Parse(Console.ReadLine()); // Converts the guess from string to int
-
-if (RandomNumber == PickedNumber)
+try
 {
-    Console.WriteLine("You Guessed The Number!");
+  int PickedNumber = Int32.Parse(Console.ReadLine()); // Converts the guess from string to int  
+
+  if (RandomNumber == PickedNumber)
+  {
+      Console.WriteLine("You Guessed The Number!");
+  }
+
+  else
+  {
+      Console.WriteLine("You Didnt Guess the Correct Number!");
+      Console.WriteLine("The Correct Number was " + RandomNumber);
+  }
+} 
+catch (FormatException)
+{
+    Console.WriteLine("Please Only Use Numbers!");
 }
 
-else
+catch (Exception e)
 {
-    Console.WriteLine("You Didnt Guess the Correct Number!");
-    Console.WriteLine("The Correct Number was " + RandomNumber);
+    Console.WriteLine("Somthing occured" + e.Message);
 }
 
